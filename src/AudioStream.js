ea58af44cb6a3342664220fbcd26bb5694e5009c
srgokulnath <srgwilet@gmail.com>
2022-03-01 20:48:39 +0530

    connected home page and audiostream page

--- src/AudioStream.js
+++ src/AudioStream.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './AudioStream.css'
 import {useReactMediaRecorder} from 'react-media-recorder'
+import { Link } from "react-router-dom"
+
 
 function AudioStream() {
     const {status, startRecording, stopRecording, mediaBlobUrl} = useReactMediaRecorder({audio:true})
@@ -10,6 +12,9 @@ function AudioStream() {
             <button onClick={startRecording}>Start Recording</button>
             <button onClick={stopRecording}>Stop Recording</button>
             <video src={mediaBlobUrl} autoplay loop controls></video>
+            <Link to='/'>
+                <button> Home </button>
+            </Link>
         </div>
     )
 }
--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,7 @@ function Home() {
       <Link to='./audiostream'>
         <button> audiostream </button>
       </Link>
+      
     </div>
   )
 }
