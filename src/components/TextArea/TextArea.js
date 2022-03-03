b2d6464d0082b7c8b07f7315abebfdc406c12cba
Jagadeesh <74336885+0-jagadeesh-0@users.noreply.github.com>
2022-03-03 19:43:16 +0530

    modifying audio-stream

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import Navbar from "../Navbar/Navbar";
 import { Link } from "react-router-dom";
 
 function TextArea() {
+
+  const [text, setText] = useState("");
+
   const downloadTxtFile = () => {
     const element = document.createElement("a");
     const file = new Blob([document.getElementById("input").value], {
@@ -16,10 +19,16 @@ function TextArea() {
   };
 
   const handle = () => {
-    const string = document.getElementById("imput").value;
-    const len = string.length;
-    if (len > 10) {
-      string.current.css = {color: "red"};
+    const ele = document.getElementById("input");
+    const sp1 = document.getElementById("sp-1");
+    const sp2 = document.getElementById("sp-2");
+
+    if (text.length < 10) {
+      sp1.innerHTML = ele.value.substring(0, 10);
+      console.log(sp1.innerHTML);
+    }
+    else {
+      sp2.innerHTML = ele.value.substring(10, text.length)
     }
   };
 
@@ -28,19 +37,27 @@ function TextArea() {
       <div className="text-areaBody">
         <h1>Text Downloader</h1>
 
-        <div className="ts">
-          <textarea
-            id="input"
-            className="input1"
-            onKeyUp={handle}
-          ></textarea>
-        </div>
+
+        <div
+          type="text"
+          id="input"
+          onChange={(e) => { setText(e.target.value) }}
+          className="input1"
+          onKeyUp={handle}
+        >
+          <span id="sp-1">
+
+          </span>
+          <span id="sp-2">
+
+          </span></div>
+
 
         <div className="buttons">
           <button onClick={downloadTxtFile} className="btn">
             Download
           </button>
-         
+
         </div>
       </div>
     </div>
